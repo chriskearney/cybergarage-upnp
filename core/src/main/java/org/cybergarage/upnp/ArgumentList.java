1e5323175219f5af5a463ec26b86f73267fc346c
ck <chris@kearneymail.com>
2017-11-11 23:27:00 -0800

    a rather crappy fix to allow this to work with the wemo inwall switches that i have

--- core/src/main/java/org/cybergarage/upnp/ArgumentList.java
+++ core/src/main/java/org/cybergarage/upnp/ArgumentList.java
@@ -1,120 +1,118 @@
 /******************************************************************
-*
-*	CyberUPnP for Java
-*
-*	Copyright (C) Satoshi Konno 2002
-*
-*	File: ArgumentList.java
-*
-*	Revision:
-*
-*	12/05/02
-*		- first revision.
-*
-******************************************************************/
+ *
+ *	CyberUPnP for Java
+ *
+ *	Copyright (C) Satoshi Konno 2002
+ *
+ *	File: ArgumentList.java
+ *
+ *	Revision:
+ *
+ *	12/05/02
+ *		- first revision.
+ *
+ ******************************************************************/
 
 package org.cybergarage.upnp;
 
 import java.util.Vector;
 
-public class ArgumentList extends Vector 
-{
-	////////////////////////////////////////////////
-	//	Constants
-	////////////////////////////////////////////////
-	
-	public final static String ELEM_NAME = "argumentList";
+public class ArgumentList extends Vector {
+    ////////////////////////////////////////////////
+    //	Constants
+    ////////////////////////////////////////////////
 
-	////////////////////////////////////////////////
-	//	Constructor
-	////////////////////////////////////////////////
-	
-	public ArgumentList() 
-	{
-	}
-	
-	////////////////////////////////////////////////
-	//	Methods
-	////////////////////////////////////////////////
-	
-	public Argument getArgument(int n)
-	{
-		return (Argument)get(n);
-	}
+    public final static String ELEM_NAME = "argumentList";
 
-	public Argument getArgument(String name)
-	{
-		int nArgs = size();
-		for (int n=0; n<nArgs; n++) {
-			Argument arg = getArgument(n);
-			String argName = arg.getName();
-			if (argName == null)
-				continue;
-			if (argName.equals(name) == true)
-				return arg;
-		}
-		return null;
-	}
+    ////////////////////////////////////////////////
+    //	Constructor
+    ////////////////////////////////////////////////
 
-	////////////////////////////////////////////////
-	//	Methods
-	////////////////////////////////////////////////
-	/**
-	 * @deprecated
-	 */
-	public void set(ArgumentList inArgList)
-	{
-		int nInArgs = inArgList.size();
-		for (int n=0; n<nInArgs; n++) {
-			Argument inArg = inArgList.getArgument(n);
-			String inArgName = inArg.getName();
-			Argument arg = getArgument(inArgName);
-			if (arg == null)
-				continue;
-			arg.setValue(inArg.getValue());
-		}
-	}
+    public ArgumentList() {
+    }
 
-	/**
-	 * Set all the Argument which are Input Argoument to the given value in 
-	 * the argument list
-	 * 
-	 * @param inArgList
-	 */
-	public void setReqArgs(ArgumentList inArgList)
-	{
+    ////////////////////////////////////////////////
+    //	Methods
+    ////////////////////////////////////////////////
+
+    public Argument getArgument(int n) {
+        return (Argument) get(n);
+    }
+
+    public Argument getArgument(String name) {
         int nArgs = size();
-        for (int n=0; n<nArgs; n++) { 
+        for (int n = 0; n < nArgs; n++) {
             Argument arg = getArgument(n);
-            if (arg.isInDirection()){
-	            String argName = arg.getName();
-	            Argument inArg = inArgList.getArgument(argName);
-	            if (inArg == null)
-	                throw new IllegalArgumentException("Argument \"" + argName + "\" missing.");
-	            arg.setValue(inArg.getValue());
+            String argName = arg.getName();
+            if (argName == null)
+                continue;
+            if (argName.equals(name) == true)
+                return arg;
+        }
+        return null;
+    }
+
+    ////////////////////////////////////////////////
+    //	Methods
+    ////////////////////////////////////////////////
+
+    /**
+     * @deprecated
+     */
+    public void set(ArgumentList inArgList) {
+        int nInArgs = inArgList.size();
+        for (int n = 0; n < nInArgs; n++) {
+            Argument inArg = inArgList.getArgument(n);
+            String inArgName = inArg.getName();
+            Argument arg = getArgument(inArgName);
+            if (arg == null)
+                continue;
+            arg.setValue(inArg.getValue());
+        }
+    }
+
+    /**
+     * Set all the Argument which are Input Argoument to the given value in
+     * the argument list
+     *
+     * @param inArgList
+     */
+    public void setReqArgs(ArgumentList inArgList) {
+        int nArgs = size();
+        for (int n = 0; n < nArgs; n++) {
+            Argument arg = getArgument(n);
+            if (arg.isInDirection()) {
+                String argName = arg.getName();
+                Argument inArg = inArgList.getArgument(argName);
+                if (inArg == null)
+                    throw new IllegalArgumentException("Argument \"" + argName + "\" missing.");
+                arg.setValue(inArg.getValue());
             }
         }
-	}
-	/**
-	 * Set all the Argument which are Output Argoument to the given value in 
-	 * the argument list
-	 * 
-	 * @param outArgList
-	 */
-	public void setResArgs(ArgumentList outArgList)
-	{
+    }
+
+    /**
+     * Set all the Argument which are Output Argoument to the given value in
+     * the argument list
+     *
+     * @param outArgList
+     */
+    public void setResArgs(ArgumentList outArgList) {
         int nArgs = size();
-        for (int n=0; n<nArgs; n++) {
+        for (int n = 0; n < nArgs; n++) {
             Argument arg = getArgument(n);
-            if (arg.isOutDirection()){
-	            String argName = arg.getName();
-	            Argument outArg = outArgList.getArgument(argName);
-	            if (outArg == null)
-	                throw new IllegalArgumentException("Argument \"" + argName + "\" missing.");
-	            arg.setValue(outArg.getValue());
+            // if (arg.isOutDirection()){
+            // dirty hack to make this work with the wemo inwall switches
+            // something is f'd up with a OUT arg that comes back. I dont care about it for my simple library, so im not proceeding with any out args at all
+            if (false) {
+                String argName = arg.getName();
+                Argument outArg = outArgList.getArgument(argName);
+                if (outArg == null)
+                    throw new IllegalArgumentException("Argument \"" + argName + "\" missing.");
+                arg.setValue(outArg.getValue());
             }
         }
-	}
+    }
 
 
 }
